5e2e5d0e3bf06e03e3cc2a1e24b2676f09fc2440
lakshmi-k05 <kplakshmi.97@gmail.com>
2018-04-18 17:03:29 +0530

    First upload

---
+++ PaintClient.java
@@ -0,0 +1,11 @@
+package Paint;
+import java.awt.*;
+import javax.swing.*;
+
+public class PaintClient{
+
+    public static void main(String args[])
+    {
+        
+    }
+}
---
+++ WindowCreator.java
@@ -0,0 +1,18 @@
+package Paint;
+import javax.swing.*;
+
+public class WindowCreator extends JPanel{
+	public WindowCreator()
+	{
+		super("Hello there");
+	}
+	public void paint (Graphics g)
+    {
+    	
+    	ShapeColor c = new Red("red");
+    	ShapeColor c2 = new Blue("blue");
+    	Shape sq = new Rectangle(600,100,344, 800);
+    	sq.setColor(c,c2);
+    	sq.draw(g);
+    }
+}	
